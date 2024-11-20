213559a44fa569b3a8a25636f9ca01d608883fd2
Çağrı Dingiltepe <cagridtp@gmail.com>
2024-11-20 15:14:50 +0300

    Avzi
    Avzi Game in C#

---
+++ Avzi/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Avzi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_mermi = new System.Windows.Forms.Label();
+            this.PB_Kus = new System.Windows.Forms.PictureBox();
+            this.Kus_Ucur = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Kalan_Kus = new System.Windows.Forms.Label();
+            this.Can_1 = new System.Windows.Forms.PictureBox();
+            this.Can_2 = new System.Windows.Forms.PictureBox();
+            this.Can_3 = new System.Windows.Forms.PictureBox();
+            this.Kcan1 = new System.Windows.Forms.PictureBox();
+            this.Kcan2 = new System.Windows.Forms.PictureBox();
+            this.Kcan3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Kus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Can_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Can_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Can_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kcan1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kcan2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kcan3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1271, 627);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbl_mermi
+            // 
+            this.lbl_mermi.AutoSize = true;
+            this.lbl_mermi.Font = new System.Drawing.Font("Century Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_mermi.Location = new System.Drawing.Point(617, 561);
+            this.lbl_mermi.Name = "lbl_mermi";
+            this.lbl_mermi.Size = new System.Drawing.Size(77, 57);
+            this.lbl_mermi.TabIndex = 1;
+            this.lbl_mermi.Text = "20";
+            // 
+            // PB_Kus
+            // 
+            this.PB_Kus.Image = ((System.Drawing.Image)(resources.GetObject("PB_Kus.Image")));
+            this.PB_Kus.Location = new System.Drawing.Point(1123, 267);
+            this.PB_Kus.Name = "PB_Kus";
+            this.PB_Kus.Size = new System.Drawing.Size(107, 72);
+            this.PB_Kus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_Kus.TabIndex = 2;
+            this.PB_Kus.TabStop = false;
+            this.PB_Kus.Click += new System.EventHandler(this.PB_Kus_Click);
+            // 
+            // Kus_Ucur
+            // 
+            this.Kus_Ucur.Interval = 1;
+            this.Kus_Ucur.Tick += new System.EventHandler(this.Kus_Ucur_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kalan Kuş Sayısı :";
+            // 
+            // Kalan_Kus
+            // 
+            this.Kalan_Kus.AutoSize = true;
+            this.Kalan_Kus.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Kalan_Kus.Location = new System.Drawing.Point(268, 9);
+            this.Kalan_Kus.Name = "Kalan_Kus";
+            this.Kalan_Kus.Size = new System.Drawing.Size(47, 34);
+            this.Kalan_Kus.TabIndex = 4;
+            this.Kalan_Kus.Text = "16";
+            // 
+            // Can_1
+            // 
+            this.Can_1.Image = ((System.Drawing.Image)(resources.GetObject("Can_1.Image")));
+            this.Can_1.Location = new System.Drawing.Point(1093, 12);
+            this.Can_1.Name = "Can_1";
+            this.Can_1.Size = new System.Drawing.Size(38, 24);
+            this.Can_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Can_1.TabIndex = 5;
+            this.Can_1.TabStop = false;
+            // 
+            // Can_2
+            // 
+            this.Can_2.Image = ((System.Drawing.Image)(resources.GetObject("Can_2.Image")));
+            this.Can_2.Location = new System.Drawing.Point(1137, 12);
+            this.Can_2.Name = "Can_2";
+            this.Can_2.Size = new System.Drawing.Size(38, 24);
+            this.Can_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Can_2.TabIndex = 5;
+            this.Can_2.TabStop = false;
+            // 
+            // Can_3
+            // 
+            this.Can_3.Image = ((System.Drawing.Image)(resources.GetObject("Can_3.Image")));
+            this.Can_3.Location = new System.Drawing.Point(1181, 12);
+            this.Can_3.Name = "Can_3";
+            this.Can_3.Size = new System.Drawing.Size(38, 24);
+            this.Can_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Can_3.TabIndex = 5;
+            this.Can_3.TabStop = false;
+            // 
+            // Kcan1
+            // 
+            this.Kcan1.Image = ((System.Drawing.Image)(resources.GetObject("Kcan1.Image")));
+            this.Kcan1.Location = new System.Drawing.Point(1093, 13);
+            this.Kcan1.Name = "Kcan1";
+            this.Kcan1.Size = new System.Drawing.Size(38, 24);
+            this.Kcan1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Kcan1.TabIndex = 6;
+            this.Kcan1.TabStop = false;
+            // 
+            // Kcan2
+            // 
+            this.Kcan2.Image = ((System.Drawing.Image)(resources.GetObject("Kcan2.Image")));
+            this.Kcan2.Location = new System.Drawing.Point(1137, 12);
+            this.Kcan2.Name = "Kcan2";
+            this.Kcan2.Size = new System.Drawing.Size(38, 25);
+            this.Kcan2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Kcan2.TabIndex = 7;
+            this.Kcan2.TabStop = false;
+            // 
+            // Kcan3
+            // 
+            this.Kcan3.Image = ((System.Drawing.Image)(resources.GetObject("Kcan3.Image")));
+            this.Kcan3.Location = new System.Drawing.Point(1181, 11);
+            this.Kcan3.Name = "Kcan3";
+            this.Kcan3.Size = new System.Drawing.Size(38, 25);
+            this.Kcan3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Kcan3.TabIndex = 8;
+            this.Kcan3.TabStop = false;
+            this.Kcan3.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 627);
+            this.Controls.Add(this.Kcan1);
+            this.Controls.Add(this.Kcan2);
+            this.Controls.Add(this.Kcan3);
+            this.Controls.Add(this.Can_3);
+            this.Controls.Add(this.Can_2);
+            this.Controls.Add(this.Can_1);
+            this.Controls.Add(this.Kalan_Kus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PB_Kus);
+            this.Controls.Add(this.lbl_mermi);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Kus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Can_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Can_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Can_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kcan1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kcan2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kcan3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_mermi;
+        private System.Windows.Forms.PictureBox PB_Kus;
+        private System.Windows.Forms.Timer Kus_Ucur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Kalan_Kus;
+        private System.Windows.Forms.PictureBox Can_1;
+        private System.Windows.Forms.PictureBox Can_2;
+        private System.Windows.Forms.PictureBox Can_3;
+        private System.Windows.Forms.PictureBox Kcan1;
+        private System.Windows.Forms.PictureBox Kcan2;
+        private System.Windows.Forms.PictureBox Kcan3;
+    }
+}
+
---
+++ Avzi/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Avzi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PB_Kus.Width = 75;
+            PB_Kus.Height = 50;
+
+            lbl_mermi.Parent = pictureBox1;
+            lbl_mermi.BackColor = Color.Transparent;
+
+            PB_Kus.Parent = pictureBox1;
+            PB_Kus.BackColor = Color.Transparent;
+
+            label1.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+
+            Kalan_Kus.Parent = pictureBox1;
+            Kalan_Kus.BackColor = Color.Transparent;
+
+            Can_1.Parent = pictureBox1;
+            Can_1.BackColor = Color.Transparent;
+
+            Can_2.Parent = pictureBox1;
+            Can_2.BackColor = Color.Transparent;
+
+            Can_3.Parent = pictureBox1;
+            Can_3.BackColor = Color.Transparent;
+        }
+
+        int mermi = 20;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            mermi--;
+            lbl_mermi.Text = mermi.ToString();
+
+            if (mermi == 0)
+            {
+                Kus_Ucur.Enabled = false;
+                MessageBox.Show("Vurulmayan kuş sayısı:" + vurulmayan.ToString() + "\n Kalan kuş sayısı:" + kalanKus.ToString(), "MERMİ BİTTİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+
+            
+        }
+
+        int x, y;
+        Random r = new Random();
+
+        int vurulmayan = 0;
+        int can = 3;
+        private void Kus_Ucur_Tick(object sender, EventArgs e)
+        {
+            y -= 2;
+
+            PB_Kus.SetBounds(x, y, 75, 50);
+
+            if(y < 10)
+            {
+                x = r.Next(880);
+                y = 230;
+
+                PB_Kus.SetBounds(x, y, 75, 50);
+
+                kalanKus--;
+                Kalan_Kus.Text = kalanKus.ToString();
+
+                vurulmayan++;
+
+                can--;
+
+                if (can == 2)
+                {
+                    this.Controls.Remove(Kcan3);
+                }
+
+                if (can == 1)
+                {
+                    this.Controls.Remove(Kcan2);
+                }
+
+                if (can == 0)
+                {
+                    this.Controls.Remove(Kcan1);
+                    Kus_Ucur.Stop();
+                    MessageBox.Show("Vurulmayan kuş sayısı:" + vurulmayan.ToString() + "\n Kalan kuş sayısı:" + kalanKus.ToString(), "KAYBETTİNİZ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+            }
+
+            /*
+            if (kalanKus == 0)
+            {
+                Kus_Ucur.Enabled = false;
+                if(vurulmayan == 0)
+                {
+                    MessageBox.Show("Vurulmayan kuş sayısı:" + vurulmayan.ToString() + "\n TEKBRİKLER KAZANDINIZ", "KAZANDINIZ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                else
+                {
+                    MessageBox.Show("Vurulmayan kuş sayısı:" + vurulmayan.ToString() + "\n MAALESEF KAYBETTİNİZ", "KAYBETTİNİZ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                this.Close();
+            } */
+        }
+
+        int kalanKus = 16;
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PB_Kus_Click(object sender, EventArgs e)
+        {
+            kalanKus--;
+            Kalan_Kus.Text = kalanKus.ToString();
+
+            if (kalanKus == 0)
+            {
+                Kus_Ucur.Enabled = false;
+                MessageBox.Show("Vurulmayan kuş sayısı:" + vurulmayan.ToString() + "\n Kalan kuş sayısı:" + kalanKus.ToString(), "KAZNDINIZ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+
+            mermi--;
+            lbl_mermi.Text = mermi.ToString();    
+            
+            x = r.Next(880);
+            y = 230; 
+
+            PB_Kus.SetBounds(x, y, 75, 50);
+
+            Kus_Ucur.Enabled = true;
+            
+        }
+    }
+}
